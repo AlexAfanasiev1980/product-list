4306183b15e6f18048066fd72155169f207d0b70
Alexander Afanasiev <armawir1@mail.ru>
2024-02-26 15:49:19 +0700

    delete button for get products

--- src/App.tsx
+++ src/App.tsx
@@ -6,47 +6,45 @@ import {
 } from "./store/features/products/productSlice";
 import { useAppDispatch, useAppSelector } from "./store/store";
 import { PaginationPage } from "./pages/paginationPage/PaginationPage";
+import { useEffect } from "react";
 
 function App({ itemsPerPage }: { itemsPerPage: number }) {
   const dispatch = useAppDispatch();
   const productsId = useAppSelector((store) => store.products.productsId);
   const loading = useAppSelector((store) => store.products.loading);
-
   const date = getDate();
 
-  const getProducts = async () => {
-    dispatch(setLoading(true));
-    try {
-      const listId: any = await dispatch(
-        fetchProductsId({
-          password: `Valantis_${date}`,
-          offset: 0,
-          limit: itemsPerPage,
-        })
-      );
-      if (listId.error) {
+  useEffect(() => {
+    const getId = async () => {
+      dispatch(setLoading(true));
+      try {
+        const listId: any = await dispatch(
+          fetchProductsId({
+            password: `Valantis_${date}`,
+            offset: 0,
+            limit: itemsPerPage,
+          })
+        );
+        if (listId.error) {
+          dispatch(setLoading(false));
+          alert("Ошибка сервера. Обновите страницу.");
+        }
+        console.log(listId);
+      } catch (error) {
+        console.log(error);
         dispatch(setLoading(false));
-        alert('Ошибка сервера. Попробуйте еще раз.')
       }
-      console.log(listId)
-    } catch (error) {
-      console.log(error);
-      dispatch(setLoading(false));
+    };
+
+    if (productsId.length === 0) {
+      getId();
     }
-  };
+  }, [productsId]);
 
   return (
     <div className="App">
       {productsId.length === 0 ? (
-        <div className="buttonItem">
-          {loading ? (
-            <div>Loading ...</div>
-          ) : (
-            <button className="button" onClick={getProducts}>
-              Получить список продуктов
-            </button>
-          )}
-        </div>
+        <div className="buttonItem">{loading && <div>Loading ...</div>}</div>
       ) : (
         <PaginationPage itemsPerPage={itemsPerPage} date={date} />
       )}
